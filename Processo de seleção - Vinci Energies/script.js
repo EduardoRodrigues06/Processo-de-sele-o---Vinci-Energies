558d1fb378b72a47b15a4eb16064c4ed12e216e2
EduardoRodrigues06 <154531506+EduardoRodrigues06@users.noreply.github.com>
2024-03-29 02:13:33 -0300

    Add files via upload

---
+++ Processo de seleção - Vinci Energies/script.js
@@ -0,0 +1,21 @@
+function ValidarForm() {
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var mensagem = document.getElementById("mensagem").value;
+    if (nome == "" || email == "" || mensagem == "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Por favor, insira um endereço de e-mail válido.");
+        return false;
+    }
+    alert("E-mail enviado com sucesso!");
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("mensagem").value = "";
+    return true;
+}
+
+
